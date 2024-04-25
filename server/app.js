c6bf219b623a61182b0f42ed546ca958a33af7d3
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-04-25 07:13:46 +0530

    initial commit: graphql req works

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
+const PORT = 3000;
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server heard at http://localhost:${PORT}`);
+});
