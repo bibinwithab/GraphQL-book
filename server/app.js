1deab255dd71f62263fd34bf9be6a48d6d8c76c2
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-04-26 07:23:27 +0530

    build: connected to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const { mongoose } = require("mongoose");
+require("dotenv").config();
 
 const PORT = 3000;
 const app = express();
@@ -15,4 +17,8 @@ app.use(
 
 app.listen(PORT, () => {
   console.log(`Server heard at http://localhost:${PORT}`);
+
+  mongoose.connect(process.env.DB_URI).then(() => {
+    console.log("Connected to database");
+  });
 });
