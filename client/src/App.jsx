6ea3a13619072505d104718e4f45e401b9c5222a
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-05-07 19:24:09 +0530

    build: client fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import ShowBooks from "./components/ShowBooks"
+import ShowBooks from "./components/ShowBooks";
 
 function App() {
   return (
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-red-600 to-fuchsia-600">
-      <div className="absolute inset-0 backdrop-filter backdrop-blur-lg bg-opacity-50"></div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="absolute inset-0 bg-gradient-to-b from-violet-500 to-purple-900 opacity-80"></div>
       <div className="relative z-10">
-        <h1 className="text-3xl font-bold text-center mb-8">Books! A lot of 'em</h1>
+        <h1 className="text-3xl font-bold text-center mb-8 text-white">Books! A lot of 'em</h1>
         <ShowBooks />
       </div>
     </div>
--- client/src/components/ShowBooks.jsx
+++ client/src/components/ShowBooks.jsx
@@ -4,16 +4,16 @@ import { useQuery } from "@apollo/client";
 const ShowBooks = () => {
   const { data, loading, error } = useQuery(BOOK_QUERY);
 
-  if (loading) return <p className="text-3xl font-bold text-center m-8">Loading...</p>;
-  if (error) return <p className="text-3xl font-bold text-center m-8">Error: {error.message}</p>;
+  if (loading) return <p className="text-3xl font-bold text-center text-white m-8">Loading...</p>;
+  if (error) return <p className="text-3xl font-bold text-center text-white m-8">Error: {error.message}</p>;
 
   return (
     <div className="grid grid-cols-4 gap-4">
       {data?.books.map((book) => (
-        <div className="flex flex-col items-center justify-center px-6 py-4 bg-amber-400 border-0 rounded-md hover:bg-amber-500 cursor-pointer" key={book.id}>
+        <div className="flex flex-col items-center justify-center px-6 py-4 bg-teal-500 border-0 rounded-md hover:bg-teal-600 cursor-pointer" key={book.id}>
           <h2 className="text-fuchsia-800 font-bold uppercase ">Name: {book.name}</h2>
-          <p className="text-slate-700  uppercase">Author Name: {book.author.name}</p>
-          <p className="text-fuchsia-900  uppercase">Genre: {book.genre}</p>
+          <p className="text-slate-300  uppercase">Author Name: {book.author.name}</p>
+          <p className="text-indigo-900  uppercase">Genre: {book.genre}</p>
 
         </div>
       ))}
