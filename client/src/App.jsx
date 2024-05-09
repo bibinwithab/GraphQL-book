88e64e1f75a350336caa0f29d2353537cdbdb04f
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-05-09 14:08:39 +0530

    build: frontend styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import ShowBooks from "./components/ShowBooks";
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="absolute inset-0 bg-gradient-to-b from-violet-500 to-purple-900 opacity-80"></div>
+      <div className="absolute inset-0 bg-gradient-to-br from-slate-900 to-purple-700 opacity-80"></div>
       <div className="relative z-10">
         <h1 className="text-3xl font-bold text-center mb-8 text-white">Books! A lot of 'em</h1>
         <ShowBooks />
--- client/src/components/ShowBooks.jsx
+++ client/src/components/ShowBooks.jsx
@@ -12,7 +12,7 @@ const ShowBooks = () => {
       {data?.books.map((book) => (
         <div className="flex flex-col items-center justify-center px-6 py-4 bg-teal-500 border-0 rounded-md hover:bg-teal-600 cursor-pointer" key={book.id}>
           <h2 className="text-fuchsia-800 font-bold uppercase ">Name: {book.name}</h2>
-          <p className="text-slate-300  uppercase">Author Name: {book.author.name}</p>
+          <p className="text-slate-200  uppercase">Author Name: {book.author.name}</p>
           <p className="text-indigo-900  uppercase">Genre: {book.genre}</p>
 
         </div>
